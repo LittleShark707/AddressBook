f3ef6346d16f90bef22d94aaa6ada748c2cf686f
LittleShark707 <86358047+LittleShark707@users.noreply.github.com>
2021-06-24 14:24:44 +0800

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace midterm.Properties
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        string nameerror = "- Name should have at least five (5) characters\n";
+        string doberror = "- Date must follow the correct format (MM/DD/YYYY)\n";
+        string pinerror = "- Pin must contain six (6) numeric characters\n";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool ntrue = false;
+            bool dtrue = false;
+            bool ptrue = false;
+
+            if (name.Text.Length >= 5)
+            {
+                ntrue = true;
+            }
+            if (!dob.Text.Any(char.IsLetter) && dob.Text.Length == 10) {
+                DateTime db = DateTime.ParseExact(dob.Text, "MM/dd/yyyy", null);
+                string date = db.ToString("MM/dd/yyyy");
+                if (dob.Text == date)
+                {
+                    dtrue = true;
+                }
+            }
+            else if (dob.Text.Any(char.IsLetter))
+            {
+                dtrue = false;
+            }
+
+            if (pin.Text.All(char.IsDigit) && pin.Text != "")
+            {
+                ptrue = true;
+            }
+
+            if(!ntrue && !dtrue && !ptrue)
+            {
+                MessageBox.Show(nameerror+doberror+pinerror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (!ntrue && !dtrue && ptrue)
+            {
+                MessageBox.Show(nameerror + doberror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (!ntrue && dtrue && ptrue)
+            {
+                MessageBox.Show(nameerror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (ntrue && !dtrue && !ptrue)
+            {
+                MessageBox.Show(doberror+pinerror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (ntrue && dtrue && !ptrue)
+            {
+                MessageBox.Show(pinerror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (!ntrue && dtrue && !ptrue)
+            {
+                MessageBox.Show(nameerror + pinerror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (ntrue && !dtrue && ptrue)
+            {
+                MessageBox.Show(doberror, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if(ntrue && dtrue && ptrue)
+            {
+
+                string path = @"C:\Users\Natasja\Desktop\privatebook.ini";
+                string ini = name.Text + "\n" + pin.Text + "\n" + dob.Text;
+                File.WriteAllText(path, ini);
+
+                Window2 w2 = new Window2();
+                this.Close();
+                w2.Show();
+            }
+
+            pin.IsEnabled = true;
+        }
+
+        private void pin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (pin.Text != "")
+            {
+                l3.Content = " ";
+            }
+            else if (pin.Text == "")
+            {
+                l3.Content = "*";
+            }
+
+            if (pin.Text.Length >= 6)
+            {
+                pin.IsEnabled = false;
+            }
+        }
+
+        private void dob_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (dob.Text != "")
+            {
+                l2.Content = " ";
+            }
+            else if (dob.Text == "")
+            {
+                l2.Content = "*";
+            }
+
+            dob.SelectionStart = dob.Text.Length;
+            if (dob.Text.Length == 2 && dob.Text.All(char.IsDigit))
+            {
+                string s = dob.Text;
+                dob.Text = s + "/";
+            }
+
+            if (dob.Text.Length == 5 && !(dob.Text.Any(char.IsLetter)))
+            {
+                string s = dob.Text;
+                dob.Text = s + "/";
+            }
+        }
+
+        private void name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (name.Text != "")
+            {
+                l1.Content = " ";
+            }
+            else if (name.Text == "")
+            {
+                l1.Content = "*";
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            dob.Text = "";
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace midterm.Properties
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0, num6 = 0;
+        string path = @"C:\Users\Natasja\Desktop\privatebook.ini";
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void t1_Click(object sender, RoutedEventArgs e)
+        {
+            num1 += 1;
+            if (num1 > 9)
+            {
+                num1 = 0;
+            }
+            else if(num1 < 0)
+            {
+                num1 = 9;
+            }
+            n1.Content = num1.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void t2_Click(object sender, RoutedEventArgs e)
+        {
+            num2 += 1;
+            if (num2 > 9)
+            {
+                num2 = 0;
+            }
+            else if (num2 < 0)
+            {
+                num2 = 9;
+            }
+            n2.Content = num2.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            num2 -= 1;
+            if (num2 > 9)
+            {
+                num2 = 0;
+            }
+            else if (num2 < 0)
+            {
+                num2 = 9;
+            }
+            n2.Content = num2.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            num3 -= 1;
+            if (num3 > 9)
+            {
+                num3 = 0;
+            }
+            else if (num3 < 0)
+            {
+                num3 = 9;
+            }
+            n3.Content = num3.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void b4_Click(object sender, RoutedEventArgs e)
+        {
+            num4 -= 1;
+            if (num4 > 9)
+            {
+                num4 = 0;
+            }
+            else if (num4 < 0)
+            {
+                num4 = 9;
+            }
+            n4.Content = num4.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void b5_Click(object sender, RoutedEventArgs e)
+        {
+            num5 -= 1;
+            if (num5 > 9)
+            {
+                num5 = 0;
+            }
+            else if (num5 < 0)
+            {
+                num5 = 9;
+            }
+            n5.Content = num5.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void b6_Click(object sender, RoutedEventArgs e)
+        {
+            num6 -= 1;
+            if (num6 > 9)
+            {
+                num6 = 0;
+            }
+            else if (num6 < 0)
+            {
+                num6 = 9;
+            }
+            n6.Content = num6.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void t3_Click(object sender, RoutedEventArgs e)
+        {
+            num3 += 1;
+            if (num3 > 9)
+            {
+                num3 = 0;
+            }
+            else if (num3 < 0)
+            {
+                num3 = 9;
+            }
+            n3.Content = num3.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+            + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void t4_Click(object sender, RoutedEventArgs e)
+        {
+            num4 += 1;
+            if (num4 > 9)
+            {
+                num4 = 0;
+            }
+            else if (num4 < 0)
+            {
+                num4 = 9;
+            }
+            n4.Content = num4.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void t5_Click(object sender, RoutedEventArgs e)
+        {
+            num5 += 1;
+            if (num5 > 9)
+            {
+                num5 = 0;
+            }
+            else if (num5 < 0)
+            {
+                num5 = 9;
+            }
+            n5.Content = num5.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void t6_Click(object sender, RoutedEventArgs e)
+        {
+            num6 += 1;
+            if (num6 > 9)
+            {
+                num6 = 0;
+            }
+            else if (num6 < 0)
+            {
+                num6 = 9;
+            }
+            n6.Content = num6.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            num1 -= 1;
+            if (num1 > 9)
+            {
+                num1 = 0;
+            }
+            else if (num1 < 0)
+            {
+                num1 = 9;
+            }
+            n1.Content = num1.ToString();
+
+            string pin = File.ReadLines(path).ElementAtOrDefault(1);
+            string PIN = n1.Content.ToString() + n2.Content.ToString() + n3.Content.ToString()
+                + n4.Content.ToString() + n5.Content.ToString() + n6.Content.ToString();
+            if (PIN == pin)
+            {
+                this.Close();
+            }
+        }
+    }
+}
